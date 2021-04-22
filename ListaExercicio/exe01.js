712f4a679405c139b9bedf30c08fd05e13f1c04d
danielduartedasilva <danielddsilva10@gmail.com>
2021-04-22 03:17:27 -0300

    feito exercicio 02

--- ListaExercicio/exe01.js
+++ ListaExercicio/exe01.js
@@ -1,1 +1,12 @@
-console.log("Uma mensagem bonita!");
+let altura = Math.floor(Math.random() * 10);
+let largura = Math.floor(Math.random() * 10);
+let area = altura * largura;
+console.log(altura, largura, area);
+
+// Fazendo com função
+// let area = calcularArea(altura, largura);
+// console.log(altura, largura, area);
+// function calcularArea(altura, largura){
+//     return altura * largura;
+// }
+
--- ListaExercicio/exe02.js
+++ ListaExercicio/exe02.js
@@ -1,11 +1,7 @@
-let altura = Math.floor(Math.random() * 10);
-let largura = Math.floor(Math.random() * 10);
-// let area = altura * largura;
-// console.log(altura, largura, area);
-
-// Fazendo com função
-let area = calcularArea(altura, largura);
-console.log(altura, largura, area);
-function calcularArea(altura, largura){
-    return altura * largura;
-}
+let valor = Math.floor(Math.random() * 10);
+let valor1 = valor * 3.78;
+let valor2 = valor * 4.21;
+let valor3 = valor * 0.08;
+console.log(valor + " Dolar = " + "R$" + valor1 + "reais");
+console.log(valor + " Euro = " + "R$" + valor2 + "reais");
+console.log(valor + " Peso = " + "R$" + valor3 + "reais");
