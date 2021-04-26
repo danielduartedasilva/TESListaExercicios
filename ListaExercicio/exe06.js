7064be0a5dab5ded6b63036c3889fc9fc8ef7605
danielduartedasilva <danielddsilva10@gmail.com>
2021-04-26 16:53:34 -0300

    feito exercicio 06

--- ListaExercicio/exe06.js
+++ ListaExercicio/exe06.js
@@ -1,65 +1,30 @@
-let [] = Math.floor(Math.random() * 10);
-//let num2 = [0,1,2];
-//console.log(num1);
-console.log(num1);
+//Utilizando o sort do javaScript
+let num = [];
+for(let i=0; i < 5;i++){
+num.push(Math.floor(Math.random() * 10));
+}
+console.log(num);
 
+function ordenaNum(a,b) {
+     return a - b;
+}
+num.sort(ordenaNum);
+console.log(num);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// console.log("Escolha uma opção:");
-// let opcao;
-// do {
-//     console.log("---------------MENU-----------------");
-//     console.log("--   1 - Cadastrar Cliente             --");
-//     console.log("--   2 - Listar clientes                  --");
-//     console.log("--   3 - Cadastar Vendedor          --");
-//     console.log("--   4 - Listar vendedor               --");
-//     console.log("--   5 - Cadastrar produto           --");
-//     console.log("--   6 - Listar produtos                --");
-//     console.log("--   7 - Registrar venda               --");
-//     console.log("--   8 - Listar vendas                   --");
-//     console.log("--   9 - Listar vendas por cliente --");
-//     console.log("--   0 - Sair                                  --");
-// } while (opcao != 0);
-
-// if (opcao == 1) {
-//     console.log("1 - Cadastrar Cliente");
-// } else if (opcao == 2) {
-//     console.log("2 - Listar clientes");
-// } else if (opcao == 3) {
-//     console.log("3 - Cadastar Vendedor");
-// } else if (opcao == 4) {
-//     console.log("4 - Listar vendedor");
-// } else if (opcao == 5) {
-//     console.log("5 - Cadastrar produto");
-// } else if (opcao == 6) {
-//     console.log("6 - Listar produtos");
-// } else if (opcao == 7) {
-//     console.log("7 - Registrar venda");
-// } else if (opcao == 8) {
-//     console.log("8 - Listar vendas");
-// } else if (opcao == 9) {
-//     console.log("9 - Listar vendas por cliente");
-// } else {
-
+//No jeito tradicional
+// let num = [];
+// for(let i=0; i < 5;i++){
+// num.push(Math.floor(Math.random() * 10));
 // }
-
-
-let altura = Math.floor(Math.random() * 10);
-let largura = Math.floor(Math.random() * 10);
-let area = altura * largura;
-console.log(altura, largura, area);
+// console.log(num);
+// let aux;
+// for (let i = 0; i < 5; i++) {
+//      for (let j = i; j < 5; j++) {
+//            if (num[i] > num[j]) {
+//                  aux = num[i];
+//                  num[i] = num[j];
+//                  num[j] = aux;
+//           }
+//      }
+// }
+// console.log(num);
---
+++ ListaExercicio/exe07.js
@@ -0,0 +1,38 @@
+//Rascunho
+// console.log("Escolha uma opção:");
+// let opcao;
+// do {
+//     console.log("---------------MENU-----------------");
+//     console.log("--   1 - Cadastrar Cliente             --");
+//     console.log("--   2 - Listar clientes                  --");
+//     console.log("--   3 - Cadastar Vendedor          --");
+//     console.log("--   4 - Listar vendedor               --");
+//     console.log("--   5 - Cadastrar produto           --");
+//     console.log("--   6 - Listar produtos                --");
+//     console.log("--   7 - Registrar venda               --");
+//     console.log("--   8 - Listar vendas                   --");
+//     console.log("--   9 - Listar vendas por cliente --");
+//     console.log("--   0 - Sair                                  --");
+// } while (opcao != 0);
+
+// if (opcao == 1) {
+//     console.log("1 - Cadastrar Cliente");
+// } else if (opcao == 2) {
+//     console.log("2 - Listar clientes");
+// } else if (opcao == 3) {
+//     console.log("3 - Cadastar Vendedor");
+// } else if (opcao == 4) {
+//     console.log("4 - Listar vendedor");
+// } else if (opcao == 5) {
+//     console.log("5 - Cadastrar produto");
+// } else if (opcao == 6) {
+//     console.log("6 - Listar produtos");
+// } else if (opcao == 7) {
+//     console.log("7 - Registrar venda");
+// } else if (opcao == 8) {
+//     console.log("8 - Listar vendas");
+// } else if (opcao == 9) {
+//     console.log("9 - Listar vendas por cliente");
+// } else {
+
+// }
